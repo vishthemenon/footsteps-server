358755aeb49774a5362f3740f36afc88c6738b85
Vishnu R Menon <vishthemenon@gmail.com>
2016-06-25 13:53:37 +0800

    init rethinkdb

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var app = require('express')()
+var r = require('rethinkdbdash')({
+  db: 'footsteps',
+  host: 'aws-us-east-1-portal.17.dblayer.com',
+  port: 11672
+})
 
 app.listen('3000', function() {
   console.log("Shit is being served on port 3000")
+  r.table('users').filter({email: 'michelle@gmail.com'}).run().then(function(users) {
+    console.log(users[0].email)
+  })
 })
