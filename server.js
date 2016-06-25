50b1e5685375b314f327797b6ec8a9f39491e4a2
Vishnu R Menon <vishthemenon@gmail.com>
2016-06-25 15:32:32 +0800

    intelligent auth strategy

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var app = require('express')()
+var bodyParser = require('body-parser')
 var r = require('rethinkdbdash')({
   db: 'footsteps',
   host: 'ec2-54-169-219-166.ap-southeast-1.compute.amazonaws.com',
   port: 28015
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/users', function(req, res) {
   r.table('users').run().then(function(result) {
@@ -14,6 +18,17 @@ app.get('/users', function(req, res) {
 
 app.get('/user/:id', function(req, res) {
   r.table('users').get(req.params.id).run().then(function(result) {
+    if(result === null) {
+      res.send('user not found')
+    }
+    else {
+      res.send(result)
+    }
+  })
+})
+
+app.post('/user/session/new', function(req, res) {
+  r.table('users').filter({email: req.body.email}).run().then(function(result) {
     if(result === null) {
       res.send('user not found')
     }
